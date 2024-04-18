eb32bee19d7db7f45914956892db2593446bc041
Nora Solvang <norasol@stud.ntnu.no>
2024-04-18 18:29:37 +0200

    something that works

--- closed_loop_python_maybe.py
+++ closed_loop_python_maybe.py
@@ -168,7 +168,18 @@ def update_wo(u_guess_0, x0_init):
         w0 += u_guess_0.tolist() 
         w0 += x0_init.tolist()  # CONVERTING BOTH TO LIST FOR IT TO BECOME ONEBIG ARRAY...
     return w0
-print("Here is w0 before the for-loop: ", len(w0), w0)
+#print("Here is w0 before the for-loop: ", len(w0), w0)
+def update_lbw_ubw(x0_init):
+    lbw=[]
+    ubw=[]
+    lbw+=x0_init.tolist()
+    ubw+=x0_init.tolist()
+    for k in range(N):
+        lbw += [0,0,0,0] #dette er grensene for u (here it is taken into account that there ar 4 u's)
+        ubw += [1,1,1,1]
+        lbw += [40.0/90, 40.0/90, 40.0/90, 30.0/90 ] # temperatur av TES skal eeeeeeeeegt ikke gå lavere enn 65 men tester dette....
+        ubw += [90.0/90, 90.0/90, 90.0/90, 90.0/90 ]
+    return lbw,ubw
 
 
 #print("Size of w0:", len(w0))
@@ -193,28 +204,33 @@ for i in range(N):
     #solver skal ha lengdte på 1156 inn som w0,lbw og ubw, mens lbg og ubg skal ha leNgde 576
     sol = solver(x0=w0, lbx=lbw, ubx=ubw, lbg=lbg, ubg=ubg) ###################
     w_opt = sol['x'].full().flatten()
-    #print("w_opt før u_guess: ", len(w_opt),w_opt)
+    print("w_opt før u_guess: ", len(w_opt),w_opt[0:29])
     #TRYING A NEW U-guess!!!!
     #u-guess=w_opt[0:3]
-    u_guess = w_opt[: num_controls * N].reshape(N, num_controls).T
+    u_guess = w_opt[: num_controls * N].reshape(N, num_controls).T 
     #print("u_guess consisting of w_opt: ", len(u_guess),u_guess)
    
     x_guess = w_opt[num_controls * N :].reshape(N + 1, num_states).T
-    print("x_guess consisting of w_opt",len(x_guess),x_guess)
+    #print("x_guess consisting of w_opt",len(x_guess),x_guess)
     
     #print("New final state results, x_guess", len(x_guess.T[0]), x_guess.T[0])
-    control_results.append(u_guess[:, :4][0]) 
+    print(w_opt[4:8])
+    #This is the correct u_guess!!!!!
+    control_results.append(w_opt[4:8]) 
     #control_results.append(u_guess[:,0]) #her er det fire stykker men de ser jo like ut og endres ikke så ye
     #print("Type of x0_init:", type(x0_init), x0_init)
     #print("Type of u_guess:", type(u_guess[:, 0]),u_guess[:, 0])
-    plantd_next=plant(x0_init,u_guess[:, :4][0], F)
+    plantd_next=plant(x0_init,w_opt[4:8], F)
+
     x0_init=np.array(plantd_next).flatten() #to get x0_init on the right format as a [ 1 2 3]
     #final_state_results.append(x0_init)
-    u_guess_0=u_guess[:, :4][0]
+    #u_guess_0=u_guess[:, :4][0]
+    u_guess_0=w_opt[4:8]
     #print("Type of x0_init:", type(x0_init), x0_init)
     #print("Type of u_guess:", type(u_guess[:, :4][0]),u_guess[:, :4][0])
     #se om oppdateringen er riktig!!!
     w0=update_wo(u_guess_0,x0_init)
+    lbw,ubw= update_lbw_ubw(x0_init)
     #print("parts of w0 during loop: ", w0)
     #print("Here is the newest w0: ", w0)
 #print("W0 after the last forloop: ", len(w0), w0)  
@@ -229,6 +245,7 @@ x1_opt = final_state_results_done[0::4]
 x2_opt = final_state_results_done[1::4]
 x3_opt=final_state_results_done[2::4]
 x4_opt=final_state_results_done[3::4]
+
 control_results_done = [item for sublist in control_results for item in sublist]
 u1_opt = control_results_done[0::4]
 u2_opt = control_results_done[1::4]
